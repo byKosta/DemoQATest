869e9db6172ff66e0b6e81be66777e7d190fc670
@byKosta <rezerfpo4ta@mail.ru>
2023-06-11 17:16:28 +0200

    updating config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  chromeWebSecurity: false,
   defaultCommandTimeout: 10000,
    e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  
+  video: false,
+   reporter: 'junit',
+   reporterOptions: {
+        mochaFile: 'reports/test-results-[hash].xml',
+  },
 });
