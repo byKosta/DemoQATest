7c47c1bd2d29728331f56f985a45569c367db59f
@byKosta <rezerfpo4ta@mail.ru>
2023-06-11 17:41:54 +0200

    refac

--- cypress/e2e/Elements.cy.js
+++ cypress/e2e/Elements.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
-import elements from '../fixtures/elements.json'
+
 
 
 describe('first test', () => {
   it('check elements', () => {
       cy.visit('https://demoqa.com/')
       cy.get('body').contains('Elements').click()
-      cy.get('div.main-header').should('have.text', elements.checkingElements)
+      cy.get('div.main-header').should('have.text', 'Elements')
 
      
       })
