3de71cf4daffa5f0ea1b4d365956544af02a72a0
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2023-06-11 17:43:36 +0200

    fixtures/unit test (#7)
    * fixtures/unit test
    
    * refac

---
+++ cypress/e2e/Elements.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+
+
+describe('first test', () => {
+  it('check elements', () => {
+      cy.visit('https://demoqa.com/')
+      cy.get('body').contains('Elements').click()
+      cy.get('div.main-header').should('have.text', 'Elements')
+
+     
+      })
+  })
