892cd5954ee2cad79899eb1f6cc7ec976b78941e
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2023-06-11 16:13:29 +0200

    updating (#1)
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * 3
    
    * 4

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Tested DEMOQA
+![QA](https://github.com/byKosta/DemoQATest/blob/master/Screenshot_500.png)
 # https://demoqa.com/
+
 # +Add Ci
--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,5 +1,11 @@
+/// <reference types="cypress" />
+import messages from '../fixtures/messages.json'
+
 describe('first test', () => {
   it('should navigate to the demoQA website', () => {
       cy.visit('https://demoqa.com/')
+      cy.on('uncaught:exception', (err, runnable) => {
+        expect(err.message).to.equal(messages.deleteConfirmMessages)
+      })
   })
 })
