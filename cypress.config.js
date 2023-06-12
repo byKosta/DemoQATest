91456450090eebbdd05aea6af771c4561c7a34be
@byKosta <rezerfpo4ta@mail.ru>
2023-06-12 21:18:41 +0200

    add allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   viewportWidth: 1920,
@@ -7,7 +8,8 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
    e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+            return config;
     },
   },
   video: false,
