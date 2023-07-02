8946cd91ee392e52e7e5a10544fb01ef38176c66
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2023-07-02 18:28:50 +0200

    pageObject (#14)

---
+++ cypress/e2e/Tests/elementsConfigure.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />   
+import HeaderAndFooter from "../../../pageObjects/HeaderAndFooter";
+
+describe('Elements Configure', () => {
+
+    const headerAndFooter = new HeaderAndFooter();
+    
+    it.only('POM Elements Configure', () => {
+        headerAndFooter
+              
+              .getElementsBtn()
+                
+              
+                 
+
+    });
+});
