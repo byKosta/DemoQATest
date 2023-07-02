3e21d2875242545c84ae8e1254a4a0b8c619e7d4
@byKosta <rezerfpo4ta@mail.ru>
2023-07-02 18:42:11 +0200

    pageObject2

--- cypress/support/globalHooks.js
+++ cypress/support/globalHooks.js
@@ -2,10 +2,10 @@
 //const PASSWORD = Cypress.env('local.admin.password');
 //const LOCAL_PORT = Cypress.env('local.port')
 
-beforeEach(() => {
-    //cy.cleanData();
-    cy.visit(`https://demoqa.com/`);
-      //cy.get('#j_username').type(USERNAME);
-      //cy.get('input[name="j_password"]').type(PASSWORD);
-      //cy.get('button[name="Submit"]').click();
-})
+//beforeEach(() => {
+  //cy.cleanData();
+  //cy.visit(`https://demoqa.com/`);
+    //cy.get('#j_username').type(USERNAME);
+    //cy.get('input[name="j_password"]').type(PASSWORD);
+    //cy.get('button[name="Submit"]').click();
+//})
