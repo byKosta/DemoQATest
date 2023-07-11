0c895f01fedbe6cc792128d2940791ef102e0529
@byKosta <rezerfpo4ta@mail.ru>
2023-07-11 12:03:01 +0200

    2

--- cypress/e2e/twotest.cy.js
+++ cypress/e2e/twotest.cy.js
@@ -2,7 +2,7 @@
 
 describe('two test', () => {
     it('verify link', () => {
-        cy.visit('https://demoqa.com/')
+        cy.visit('https://demoqa.com/');
         cy.get('div.category-cards .card-body h5').contains('Elements').click()
         cy.get('#item-0').contains('Text Box').click()
         cy.get('#userName').type('John')
@@ -12,7 +12,7 @@ describe('two test', () => {
         cy.get('#submit').click()
         cy.get('#userEmail').clear()
         cy.get('input[placeholder="name@example.com"]').type('John Doe')
-        cy.get('#submit').contains('Submit').should('be.visible')
+        cy.get('#submit').contains('Submit').should('be.visible');
 
         
         
