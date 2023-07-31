54f0f45b6db244d7fba7b8272724a878fcd79aff
@byKosta <rezerfpo4ta@mail.ru>
2023-07-31 16:24:07 +0300

    M

--- cypress/e2e/twotest.cy.js
+++ cypress/e2e/twotest.cy.js
@@ -2,7 +2,7 @@
 
 describe('two test', () => {
     it('verify link', () => {
-        cy.visit('https://demoqa.com/')
+        cy.visit('https://demoqa.com/');
         cy.get('div.category-cards .card-body h5').contains('Elements').click()
         cy.get('#item-0').contains('Text Box').click()
         cy.get('#userName').type('John')
@@ -10,9 +10,9 @@ describe('two test', () => {
         cy.get('#currentAddress').type('123 Main St')
         cy.get('#permanentAddress').type('123 Main St')
         cy.get('#submit').click()
-        cy.get('#userEmail').clear()
-        cy.get('input[placeholder="name@example.com"]').type('John Doe')
-        cy.get('#submit').contains('Submit').should('be.visible')
+        cy.get('#userEmail').clear();
+        cy.get('input[placeholder="name@example.com"]').type('John Doe');
+        cy.get('#submit').contains('Submit').should('be.visible');
 
         
         
