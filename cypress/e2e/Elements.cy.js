dcd1db20a03ed939aa560244432d279d6c9373b6
Kanstantsin Halets <89216660+byKosta@users.noreply.github.com>
2023-08-14 10:43:41 +0300

    Update Elements.cy.js (#15)

--- cypress/e2e/Elements.cy.js
+++ cypress/e2e/Elements.cy.js
@@ -4,9 +4,9 @@
 
 describe('first test', () => {
   it('check elements', () => {
-      cy.visit('https://demoqa.com/')
-      cy.get('body').contains('Elements').click()
-      cy.get('div.main-header').should('have.text', 'Elements')
+      cy.visit('https://demoqa.com/');
+      cy.get('body').contains('Elements').click();
+      cy.get('div.main-header').should('have.text', 'Elements');
 
      
       })
