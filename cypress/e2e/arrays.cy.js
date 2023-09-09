f9e941c1cbeb34bc8eb1146f7fb25c4aff6949a0
@byKosta <rezerfpo4ta@mail.ru>
2023-09-09 11:01:41 +0200

    s

--- cypress/e2e/arrays.cy.js
+++ cypress/e2e/arrays.cy.js
@@ -11,7 +11,7 @@ describe('Arrays', () => {
 
     it('Verify Meny Search', function () {
         cy.get('.search-container > input').type('New York', {force: true})
-        cy.get('button[type="submit"]').click({force: true})
+        cy.get('button[type="submit"]').click({force: true});
         cy.get('ul.search-dropdown-menu li').each(($el, index) => {
            let text = $el.text()
            if(text.includes('New York City, US')){
