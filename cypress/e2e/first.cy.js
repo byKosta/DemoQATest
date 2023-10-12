4856ada0aeba18318096e47c80806c1acccd6067
@byKosta <rezerfpo4ta@mail.ru>
2023-10-12 20:36:18 +0200

    f

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -3,9 +3,9 @@ import messages from '../fixtures/messages.json'
 
 describe('first test', () => {
   it('should navigate to the demoQA website', () => {
-      cy.visit('https://demoqa.com/')
+      cy.visit('https://demoqa.com/');
       cy.on('uncaught:exception', (err, runnable) => {
-        expect(err.message).to.equal(messages.deleteConfirmMessages)
+        expect(err.message).to.equal(messages.deleteConfirmMessages);
       })
   })
 })
