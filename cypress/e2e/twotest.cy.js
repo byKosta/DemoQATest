a8a353c89fb12f42cd1bf3e03e2ccfe3a95b0d9b
@byKosta <rezerfpo4ta@mail.ru>
2023-10-17 08:49:18 +0200

    t

--- cypress/e2e/twotest.cy.js
+++ cypress/e2e/twotest.cy.js
@@ -3,16 +3,16 @@
 describe('two test', () => {
     it('verify link', () => {
         cy.visit('https://demoqa.com/')
-        cy.get('div.category-cards .card-body h5').contains('Elements').click()
-        cy.get('#item-0').contains('Text Box').click()
-        cy.get('#userName').type('John')
-        cy.get('#userEmail').type('john@gmail')
-        cy.get('#currentAddress').type('123 Main St')
-        cy.get('#permanentAddress').type('123 Main St')
-        cy.get('#submit').click()
+        cy.get('div.category-cards .card-body h5').contains('Elements').click();
+        cy.get('#item-0').contains('Text Box').click();
+        cy.get('#userName').type('John');
+        cy.get('#userEmail').type('john@gmail');
+        cy.get('#currentAddress').type('123 Main St');
+        cy.get('#permanentAddress').type('123 Main St');
+        cy.get('#submit').click();
         cy.get('#userEmail').clear();
-        cy.get('input[placeholder="name@example.com"]').type('John Doe')
-        cy.get('#submit').contains('Submit').should('be.visible')
+        cy.get('input[placeholder="name@example.com"]').type('John Doe');
+        cy.get('#submit').contains('Submit').should('be.visible');
 
         
         
