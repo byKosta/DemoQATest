e05ce8d0ebe02aac31fc86f2c9f3671f4b58921b
byKosta <haletskonstantsin@gmail.com>
2024-05-14 13:23:33 +0200

    refactoring of all tests

--- cypress/e2e/Common-methods.cy.js
+++ cypress/e2e/Common-methods.cy.js
@@ -12,8 +12,8 @@ describe('Common methods', () => {
         cy.log('Test has been completed');
         cy.get('div.avatar').should('have.css','color','rgb(1, 160, 224)');
         cy.get('div.card-body h5').contains('Elements').click();
-        cy.get('div.main-header').should('contain', 'Elements');
-        cy.get('div.main-header').should('be.visible'); 
+        cy.get('div.header-text').should('contain', 'Elements');
+        cy.get('div.header-text').should('be.visible'); 
         cy.contains('Text Box').click();
         cy.get('input#userName')
         .should('have.attr', 'placeholder', 'Full Name')  
